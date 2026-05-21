5dc5b497c9a0fe6725c86020a84722305939b103
Dr.Colise <145095890+DRColiseo@users.noreply.github.com>
2026-05-20 19:23:47 -0600

    Add files via upload

---
+++ Promocional_FADIN/Privacidad.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="es-mx">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="FADIN, sistema, tienda, privacidad, terminos, condiciones, politicas">
+        <meta name="description" content="Página de Privacidad de FADIN">
+        <title>FADIN - Privacidad de FADIN</title>
+        <link rel="icon" href="fadinico.ico">
+        <link rel="stylesheet" href="styleFadin.css">
+    </head>
+    <body>
+    <style>
+    /* Estilo para todos los scrollbars */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track del scrollbar */
+::-webkit-scrollbar-track {
+  background: hsl(253 39% 58%); 
+}
+ 
+/* Handle del scrollbar */
+::-webkit-scrollbar-thumb {
+  background:  #0833a2;
+  border-radius: 10px;
+}
+
+/* Handle del scrollbar al pasar el mouse */
+::-webkit-scrollbar-thumb:hover {
+  background:  #FFC9C0;
+}
+</style>
+        <header>
+            <img src="porkicoin.png" alt="Logo de FADIN" title="Logo de FADIN">
+            <center><h1 style= "margin-right:580px">FADIN</h1><center>
+            <center><p style= "margin-right:580px">(Administración de dinero e inventario de negocios familiares)</p><center>
+        </header>
+        <header class="navegacion">
+            <nav>
+                <a href="index.php">FADIN</a>
+                <a href="Sobre_index.php">Sobre FADIN</a>
+                <a href="Acerca_de.php">Acerca de</a>
+            </nav>
+        </header>
+        <div class="animacion3">
+            <h2>Aviso de Privacidad</h2>
+            <p class="textopri">Estamos conscientes de la importancia que tiene para usted saber cómo utilizamos sus datos personales y le agradecemos que confíe que lo haremos con cuidado y sensatez.<br>
+                Este Aviso de Privacidad describe la forma en que “FADIN” recopila y trata sus datos personales a través de los sitios web, dispositivos, productos, servicios y aplicaciones de FADIN (conjuntamente Servicios de FADIN) que hacen referencia a este Aviso de Privacidad. Al utilizar los Servicios de FADIN, usted está dando su consentimiento a las prácticas descritas en este Aviso de Privacidad.<br>
+                Si tiene preguntas sobre como recabamos y usamos sus datos personales o sobre este aviso de privacidad, por favor póngase en contacto con nosotros. Nuestros datos de contacto son:<br>
+                Correo electrónico: ulises.hernandez1339@alumnos.udg.mx<br>
+                Atención: Responsable del Departamento de Privacidad de Datos<br>
+                A. RECOPILACIÓN DE INFORMACIÓN<br>
+                Los datos personales que recopilamos abarcan de forma general las siguientes categorías:<br>
+                Información que recopilamos automáticamente:<br>
+                Si es usted un Usuario, recopilaremos automáticamente:<br>
+                •	Una copia de cualquier correspondencia mantenida con nosotros, incluido cualquier tipo de información de verificación que solicitemos si se pone en contacto con nosotros por cualquier motivo (servicio de chat con nuestro departamento de atención al cliente o correo electrónico).<br>
+                
+                •	Detalles de operaciones y comunicaciones en la Plataforma, como los tipos y las especificaciones de los servicios, los expedientes de cualquier controversia, las comunicaciones en la Plataforma (Chat) y la información publicada en cualquier foro de discusión.
+                Podemos recopilar información sobre las personas o cuentas con las que usted comparta contenido o interactúe e información sobre contactos si decide cargarla o importarla desde algún dispositivo.
+                Información que recibimos de terceros:<br>
+                •	Terceros contratados por nosotros para prestar servicios de verificación, como las comprobaciones relativas al blanqueo de capitales, fraude y conocimiento del cliente (“Know Your Customer”) que se realizan con respecto a los Usuarios. Aquí se incluye su nombre, dirección de correo electrónico, datos e información de contacto.
+                Solo recopilaremos información personal de terceros cuando hayamos verificado que estos terceros cuentan con su consentimiento o están legalmente autorizados o obligados a revelarnos su información personal.<br>
+                B. USO DE DATOS PERSONALES<br>
+                Utilizamos sus datos personales para operar, proporcionar, desarrollar y mejorar los productos y servicios que ofrecemos a nuestros clientes. Las finalidades primarias incluyen:<br>
+                •	Proporcionar, solucionar problemas y mejorar los Servicios de “FADIN”. Utilizamos sus datos personales para proporcionar funcionalidad, analizar el rendimiento, corregir errores y mejorar la usabilidad y eficacia de los Servicios de “FADIN”.<br>
+                •	Cumplir con nuestras obligaciones legales. En algunos casos, recopilamos y utilizamos sus datos personales para cumplir con las leyes aplicables. <br>
+                •	Comunicarnos con usted. Utilizamos sus datos personales para comunicarnos con usted en relación con los Servicios de FADIN a través de diferentes canales (por ejemplo, correo electrónico, chat).<br>
+                •	Prevención de Fraude y Riesgos de Crédito. Utilizamos datos personales para prevenir y detectar fraudes y abusos con el fin de proteger la seguridad de nuestros clientes, FADIN y otros. También podemos utilizar métodos de puntuación para evaluar y gestionar los riesgos crediticios.
+                Le informamos que “FADIN”, como parte de las operaciones propias del negocio y a fin de cumplir con las finalidades descritas, podrá transferir sus datos personales a las siguientes entidades, sin que legalmente se requiera que nos otorgue su consentimiento:  <br>
+                I.	Servicio de Administración Tributaria, para cumplir con la normatividad fiscal.<br>
+                II.	Autoridades a las que debamos comunicar su información personal en virtud de una ley, con la finalidad de cumplir con la normatividad correspondiente.<br>
+                III.	Autoridades, con la finalidad de realizar las diligencias correspondientes en caso de algún incidente de seguridad.<br>
+                IV.	Empresas aseguradoras, para contar con seguros en caso de accidentes.<br>
+                V.	Empresas con las que tengamos un contrato celebrado o por celebrar, para otorgarle beneficios.<br>
+                VI.	Los casos contemplados en el artículo 37 de la Ley Federal de Protección de Datos Personales en Posesión de los Particulares.<br>
+                C. REVELACIÓN O INTERCAMBIO DE DATOS PERSONALES<br>
+                ¨FADIN¨, no realiza transferencias de información con terceros.<br>
+                Si los datos personales del usuario debieran ser compartidos con socios comerciales o patrocinadores, el usuario será notificado antes que estos sean recogidos o transferidos.<br>
+                Sólo se podrá difundir la información en casos especiales, cuando pueda servir para identificar, localizar o realizar acciones legales contra personas que pudiesen infringir las condiciones del servicio de nuestro sitio web o causar daños o interferencia sobre los derechos de ¨FADIN¨ o sus propiedades, de otros usuarios del portal o de cualquier otra persona que pudiese resultar perjudicada por dichas actividades.<br>
+                D. CONSERVACIÓN<br>
+                Conservaremos sus datos personales siempre y cuando tengamos una necesidad comercial legítima de hacerlo para prestarle servicios o suministrarle productos, tal y como exijan o requieran las leyes aplicables. Cuando no tengamos ninguna necesidad comercial legítima de tratar sus datos personales, los eliminaremos o anonimizaremos, o, cuando esto no sea posible (por ejemplo, porque sus datos personales se hayan guardado en archivos de seguridad), los guardaremos de forma segura y los mantendremos aislados para evitar un tratamiento posterior hasta que sea posible suprimirlos.<br>
+                E. SEGURIDAD<br>
+                Sabemos que la información proporcionada es importante para usted, para ello le mostramos los siguientes puntos: <br>
+                •	Diseñamos nuestros sistemas teniendo en cuenta su seguridad y privacidad.<br>
+                •	Trabajamos para proteger la transmisión de la seguridad de sus datos personales mediante el uso de protocolos de cifrado y software.<br>
+                •	Mantenemos medidas de seguridad físicas, técnicas y administrativas en relación con la recopilación, almacenamiento y divulgación de los datos personales del cliente. Nuestros procedimientos de seguridad implican que solicitemos verificar su identidad antes de que le revelemos datos personales.<br>
+                •	Es importante que usted proteja su contraseña y sus computadoras, dispositivos y aplicaciones contra cualquier acceso no autorizado. Recomendamos usar una contraseña única para su cuenta que no sea utilizada en otras cuentas en línea. <br>
+                F. MENORES DE EDAD <br>
+                La Plataforma está dirigida a adultos, y los usuarios deben tener más de 18 años para poder utilizar nuestra Plataforma. No pretendemos vender ninguno de nuestros productos o servicios a menores, según se definen en la legislación vigente. Si un menor de edad nos ha facilitado datos personales sin el consentimiento de sus padres o tutores, estos deben ponerse en contacto con ulises.hernandez1339@alumnos.udg.mx para que borremos esa información.<br>
+                I. MODIFICACIONES DE ESTA POLITICA DE PRIVACIDAD<br>
+                El presente Aviso de Privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas, por lo que nos comprometemos a mantenerlo informado sobre las modificaciones, cambios o actualizaciones que pueda sufrir el presente Aviso de Privacidad, notificándole a través de nuestro portal de internet.<br>
+                J. DERECHOS ARCO (Acceso, Rectificación, Cancelación y Oposición)<br>
+                Su información personal puede remitirse, almacenarse y procesarse en un país distinto de donde se proporcionó, lo cual se llevará a cabo de conformidad con las leyes de protección de datos aplicables. Tomamos medidas para proteger la información personal sin importar el país donde se almacena o a donde se remite. Tenemos procedimientos y controles oportunos para procurar esta protección.<br>
+                Si usted no desea que sus datos personales sean tratados para las finalidades primarias señaladas, o alguna de ellas, dé click aquí y llene el formato correspondiente conocido como “Solicitud para el ejercicio de Derechos ARCO” (Acceso, Rectificación, Cancelación y Oposición). Usted contará con un plazo de 5 días hábiles para manifestar su negativa a nuestro Departamento de Privacidad ante el tratamiento o aprovechamiento de sus datos personales para las finalidades señaladas. Su negativa no será motivo para dejar de proporcionarle nuestros servicios y realizar las demás finalidades. <br>
+                Asimismo, podrá solicitar en cualquier momento revocar el consentimiento al tratamiento de sus datos y la cancelación de su expediente, por los mismos medios.<br>
+                Su solicitud deberá ir acompañada de los argumentos por los que desea ejercer cualquiera de sus derechos, junto con una identificación oficial del titular de los datos o su apoderado, su petición será atendida en un plazo no mayor a 15 días hábiles.<br>
+                ¨FADIN¨, aconseja al usuario que actualice sus datos cada vez que estos sufran alguna modificación, ya que esto permitirá brindarle un servicio más personalizado. Si usted participa en el servicio de promociones vía correo electrónico podrá en cualquier momento desactivar el servicio.<br>
+                K. ACEPTACIÓN DE TÉRMINOS <br>
+                Si el usuario utiliza los servicios en cualquiera de los sitios de ¨FADIN¨, significa que ha leído, entendido y acordado los términos antes expuestos. Al visitar el sitio web de ¨FADIN¨, está aceptando las prácticas descritas en este Aviso de Privacidad.<br>
+                L. AUTORIDAD<br>
+                Para más información al respecto de su derecho de protección de datos personales, puede acudir al Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI), a través de su página oficial de Internet www.inai.org.mx. 
+                </p> 
+        </div>
+        <div class="animacion4">
+            <h2>T&eacute;rminos y Condiciones</h2>
+            <p class="textopri"><b>TÉRMINOS DE SERVICIO</b><br>
+                Es requisito necesario para la adquisición de los servicios que se ofrecen en este sitio, que lea y acepte los siguientes Términos y Condiciones que a continuación se redactan. El uso de nuestros servicios implicará que usted ha leído y aceptado los Términos y Condiciones de Uso en el presente documento.<br>
+                Todas los servicios que son ofrecidos por nuestro sitio web pudieran ser creadas, cobradas, enviadas o presentadas por una página web tercera y en tal caso estarían sujetas a sus propios Términos y Condiciones.<br>
+                En algunos casos, para adquirir un servicio, será necesario el registro por parte del usuario, con ingreso de datos personales fidedignos y definición de una contraseña. El usuario puede elegir y cambiar la clave para su acceso de administración de la cuenta en cualquier momento, en caso de que se haya registrado y que sea necesario para la solicitud de alguno de nuestros servicios. FADIN no asume la responsabilidad en caso de que entregue dicha clave a terceros.<br>
+                Licencia<br>
+                FADIN, a través de su sitio web concede una licencia específica por un lapso de tiempo definido para que los usuarios utilicen los servicios con el fin de lograr la acreditación de acuerdo a los Términos y Condiciones que se describen en este documento.<br>
+                Uso no autorizado<br>
+                En caso de que aplique (para venta de software, templetes, u otro producto de diseño y programación), usted no puede colocar uno de nuestros productos, modificado o sin modificar, en un CD, sitio web o ningún otro medio y ofrecerlos para la redistribución o la re-venta de ningún tipo.<br>
+                Propiedad<br>
+                Usted no puede declarar propiedad intelectual o exclusiva a ninguno de nuestros servicios, modificado o sin modificar. Todos los servicios son propiedad de los proveedores del contenido. En caso de que no se especifique lo contrario, nuestros servicios se proporcionan sin ningún tipo de garantía, expresa o implícita.<br>
+                En ningún momento esta compañía será responsable de ningún daño incluyendo, pero no limitado a, daños directos, indirectos, especiales, fortuitos o consecuentes u otras pérdidas resultantes del uso o de la imposibilidad de utilizar nuestros servicios.<br>
+                Privacidad <br>
+                FADIN, garantiza que la información personal que usted envía, cuenta con la seguridad necesaria.<br> 
+                FADIN reserva los derechos de cambiar o de modificar estos términos sin previo aviso. </p> 
+        </div>
+        <div class="separacion">
+            <center><img src="" alt="Programa FADIN"></center>
+            <center><p class="extra"><b>¿Te interesa tener un sistema FADIN personalizado?<a href="Acerca_de.php" class="azul"> Contáctanos</a></b></p></center>
+        </div>
+        <footer>
+            <div class="logo">
+                <img src="porkicoin.png" alt="Logo de FADIN" class="icono" title="Logo de FADIN">
+            </div>
+            <div class="links">
+                <h3>TEAM WORK</h3>
+                <a href="mailto:arturo.gonzalez1404@alumnos.udg.mx?">Arturo González</a><br>
+                <a href="mailto:edwin.ramos1337@alumnos.udg.mx?">Edwin Ramos</a><br>
+                <a href="mailto:melina.gonzalez1471@alumnos.udg.mx?">Melina González</a><br>
+                <a href="mailto:oscar.rivera1350@alumnos.udg.mx?">Oscar Rivera</a><br>
+                <a href="mailto:ulises.hernandez1339@alumnos.udg.mx?">Ulises Hernández</a>
+            </div>
+            <div class="links2">
+                <h3>Más de FADIN</h3>
+                <a href="index.php">FADIN</a><br>
+                <a href="Sobre_index.php">Sobre FADIN</a><br>
+                <a href="Acerca_de.php">Acerca de</a><br>
+                <a href="Privacidad.php">Términos de Privacidad</a>
+            </div>
+        </footer>
+    </body>
+</html>
